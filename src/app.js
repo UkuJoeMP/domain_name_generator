c9314cbcd03ff3fc479444c913e69dfafb0a08ca
UkuJoeMP <josemipinton@gmail.com>
2024-11-15 21:23:21 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,22 +1,17 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//write your code here
 
-window.onload = function() {
-  //write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        console.log(i[pronoun] + j[adj] + k[noun] + ".com");
-      }
+for (let SelectPronoun = 0; SelectPronoun < pronoun.length; SelectPronoun++) {
+  for (let SelectAdj = 0; SelectAdj < adj.length; SelectAdj++) {
+    for (let SelectNoun = 0; SelectNoun < noun.length; SelectNoun++) {
+      console.log(
+        pronoun[SelectPronoun] + adj[SelectAdj] + noun[SelectNoun] + ".com"
+      );
     }
   }
-};
+}
